36468aa5fed02fbae5359a33b30fff9c4bb9c0c2
elifmiraykaraman <158293589+elifmiraykaraman@users.noreply.github.com>
2025-04-04 02:09:37 +0300

    Add files via upload

---
+++ Arac.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Rota_Planlama_sistemi
+{
+    public abstract class Arac
+    {
+        public string Ad { get; set; }
+
+        public abstract decimal UcretHesapla(decimal mesafe, Yolcu yolcu);
+
+        public abstract TimeSpan SureHesapla(decimal mesafe);
+    }
+}
+
